74ec66ef4e3baa89f88a78148f0367f697197b60
Chayapol Ophaswongse <co2499@columbia.edu>
2021-03-25 15:42:16 -0400

    Make simple sightings page works

--- sightings/views.py
+++ sightings/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from .models import Sighting
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'sightings/index.html', {})
+    """
+    A view that lists all squirrel sightings with links to view each sighting
+        Located at: /sightings
+        Methods Supported: GET
+        Fields to show:
+        - Unique Squirrel ID
+        - Date
+        - Link to unique squirrel sighting
+        - This view should also have a single link to the “add” sighting view
+    """
+    sightings_all = Sighting.objects.all()
+    context = {
+        'sightings': sightings_all
+    }
+    return render(request, 'sightings/index.html', context)
+
+
+# def details(request, unique_squirrel_id):
+
+#     return render(request, 'sighting/')
