add36bbac43991e53ec6e273d56b10ae2e6fca89
Alex Libby <adl2163@columbia.edu>
2021-04-05 14:45:59 +0000

    Update Map view

--- map/views.py
+++ map/views.py
@@ -1,15 +1,15 @@
+from django.http import HttpResponse
+
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
 from sightings.models import Sighting
 
 def index(request):
-    sightings_100 = Sighting.objects.all()[:100]
+    sightings_100 = Sighting.objects.all()[:10]
     context = {
-            'sightings':sightings_100,
+            'sightings_100':sightings_100,
     }
     return render(request, 'map/index.html', context)
 
 
-
-# Create your views here.
